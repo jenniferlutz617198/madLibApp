cc02fec3abce31a480b9dffc71f8415bda0ff312
student3 <student3@jh-mbair-158-14.d214.org>
2015-12-04 14:46:26 -0600

    created viewer controllers

---
+++ madLibApp/secondViewerController.swift
@@ -0,0 +1,9 @@
+//
+//  secondViewerController.swift
+//  madLibApp
+//
+//  Created by student3 on 12/4/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import Foundation
---
+++ madLibApp/thirdViewerController.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  madLibApp
+//
+//  Created by student3 on 12/4/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import Foundation
